2523fe5bd62af1d145a1b0409d284755c980f3fc
strarsis <strarsis@gmail.com>
2020-02-26 06:41:55 +0100

    Add rounding to some tests with non-integer/irrational actual value.

--- test/test_sass.js
+++ test/test_sass.js
@@ -1,6 +1,14 @@
 var path = require('path');
 var sassTrue = require('sass-true');
+var sass = require('node-sass');
 
 var sassFile = path.join(__dirname, 'test.scss');
-sassTrue.runSass({ file: sassFile }, { describe, it });
+sassTrue.runSass({
+    file: sassFile,
+    functions: {
+        'math-round($number)': function(number) {
+            return new sass.types.Number(  Math.round(number.getValue())  );
+        }
+    }
+}, { describe, it });
 
